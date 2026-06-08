a67d558784ea8abd91a36dd4011c124c6ed53e47
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-06-08 12:32:21 +0700

    feat: add name field to TableModel and update fromJson factory method

--- lib/data/models/table.dart
+++ lib/data/models/table.dart
@@ -1,13 +1,20 @@
 class TableModel {
   final String id;
+  final String name;
   final String tableNumber;
   final String isActive;
 
-  TableModel({required this.id, required this.tableNumber, required this.isActive});
+  TableModel({
+    required this.id,
+    required this.name,
+    required this.tableNumber,
+    required this.isActive,
+  });
 
   factory TableModel.fromJson(Map<String, dynamic> json) {
     return TableModel(
       id: json['id'].toString(),
+      name: json['name'],
       tableNumber: json['table_number'].toString(),
       isActive: json['is_active'],
     );
